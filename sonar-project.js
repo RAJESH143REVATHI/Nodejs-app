b0bf0396bc2aa467c1d79ca05fda676b50e28473
RAJESH143REVATHI <rajeshthamatam94@gmail.com>
2024-12-25 20:03:08 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'sqa_68b6b7bded6c431956a52a727770a4b36478da7b',
+	    'sonar.login': 'sqa_a90a34d05090f76cd2c60491a22b14ce803f9cac',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
